6831d9aaf004afc97799eaf42b8838b84264f19c
Fraser R <frasbr@gmail.com>
2020-06-03 18:07:16 +1000

    dsadsd

--- src/pages/CreateTicket.js
+++ src/pages/CreateTicket.js
@@ -13,117 +13,127 @@ export class CreateTicket extends React.Component {
         };
     }
 
-        onFileChange = e => {
+    onFileChange = (e) => {
+        // updates the state
+        this.setState({ selectedFile: e.target.files[0] });
+    };
 
-            // updates the state
-            this.setState({ selectedFile: e.target.files[0]});
-        };
+    onFileUpload = () => {
+        const formData = new FormData();
 
-        onFileUpload = () => {
-
-            const formData = new FormData();
-
-            formData.append("myFile",
+        formData.append(
+            "myFile",
             this.state.selectedFile,
             this.setState.selectedFile.name
-            );
-            // confirmation to display in console
-            console.log(this.state.selectedFile); 
-        }
+        );
+        // confirmation to display in console
+        console.log(this.state.selectedFile);
+    };
 
-        ticket = () => {
-            const { bookingId, content, document, category} = this.state;
-            axios
-                .post("http://localhost:5000/ticket/create", {
-                    bookingId,
-                    content,
-                    document,
-                    category,
-                })
+    ticket = () => {
+        console.log(this.state);
+        // const { bookingId, content, document, category } = this.state;
+        // axios
+        //     .post("http://localhost:5000/ticket/create", {
+        //         bookingId,
+        //         content,
+        //         document,
+        //         category,
+        //     })
 
-                .then(({ data }) => {
-                    if (data.success) {
-                        // redirect
-                        document.location = "/profile"; // maybe should re-direct to previous page of where they navigated from
-                    }
-                })
-                .catch((err) => {
-                    if (err.response) {
-                        this.setState({
-                            error: err.response.data.message,
-                            isError: true,
-                        });
-                    }
-                });
-        };
-    
-        onChange = (e) => {
-            this.setState({
-                [e.target.name]: e.target.value,
-            });
-        };
-    
-        render() {
-            return (
-                <div className="base-container">
-                    <div className="header">
-                        Submit a Ticket to an Administrator 
-                    </div>
-                    <div className="content">
-                        <div className="form">
-                            <div className="form-group">
-                                <label htmlFor="filter">Ticket Type (category): </label>
+        //     .then(({ data }) => {
+        //         if (data.success) {
+        //             // redirect
+        //             document.location = "/profile"; // maybe should re-direct to previous page of where they navigated from
+        //         }
+        //     })
+        //     .catch((err) => {
+        //         if (err.response) {
+        //             this.setState({
+        //                 error: err.response.data.message,
+        //                 isError: true,
+        //             });
+        //         }
+        //     });
+    };
+
+    onChange = (e) => {
+        console.log(e.target.value);
+        console.log(this.state.category);
+        this.setState({
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    render() {
+        return (
+            <div className="base-container">
+                <div className="header">
+                    Submit a Ticket to an Administrator
+                </div>
+                <div className="content">
+                    <div className="form">
+                        <div className="form-group">
+                            <label htmlFor="filter">
+                                Ticket Type (category):{" "}
+                            </label>
                             <select
                                 name="category"
                                 onChange={this.onChange}
                                 value={this.state.category}
                             >
-                                <option value="verification">ID Verification Request</option>
+                                <option value="verification">
+                                    ID Verification Request
+                                </option>
                                 <option value="tour">Tour disputes</option>
                             </select>
+                            {this.state.category === "tour" && (
                                 <div className="form-group">
                                     <label htmlFor="content">Description</label>
-                                    {this.state.content === "tour" && (
+
                                     <textarea
                                         name="content"
                                         placeholder="input tour dispute information here.."
                                         onChange={this.onChange}
                                         value={this.state.content}
-                                    /> )}
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="document">Upload Document: </label>
-                                    <input
-                                        type="file"
-                                        name="document"
-                                        placeholder=""
-                                        onChange={this.onFileChange}
-                                        value={this.state.document}
                                     />
                                 </div>
+                            )}
+                            <div className="form-group">
+                                <label htmlFor="document">
+                                    Upload Document:{" "}
+                                </label>
+                                <input
+                                    type="file"
+                                    name="document"
+                                    placeholder=""
+                                    onChange={this.onFileChange}
+                                    value={this.state.document}
+                                />
                             </div>
                         </div>
-                        <div className="footer">
-                            <button
-                                type="button"
-                                className="submitBTN"
-                                onClick={this.ticket}
+                    </div>
+                    <div className="footer">
+                        <button
+                            type="button"
+                            className="submitBTN"
+                            onClick={this.ticket}
+                        >
+                            Submit Form
+                        </button>
+                        {this.state.isError && (
+                            <div
+                                className="error-message"
+                                style={{ color: "red" }}
                             >
-                                Submit Form
-                            </button>
-                            {this.state.isError && (
-                                <div
-                                    className="error-message"
-                                    style={{ color: "red" }}
-                                >
-                                    {this.state.error}
-                                </div>
-                            )}
-                        </div>
+                                {this.state.error}
+                            </div>
+                        )}
                     </div>
                 </div>
-            );
-        }
+            </div>
+        );
     }
+}
 
 export default CreateTicket;
